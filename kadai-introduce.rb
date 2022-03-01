5223ce5f330350c5017f45c513d4cd75aac8bf62
Kodai.Kawabe <kou7749@icloud.com>
2022-03-01 08:51:02 +0000

    change

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,6 +1,8 @@
 Myouzi = "川部"
-Namae = "航大です。"
-Nenrei = "24歳です。"
+Namae = "航大"
+Nenrei = "24"
+Toshi = "歳"
+Kanshi = "です。"
 
-Zikosyokai = Myouzi + Namae + Nenrei
+Zikosyokai = Myouzi + Namae + Kanshi + Nenrei + Toshi + Kanshi
 puts Zikosyokai
